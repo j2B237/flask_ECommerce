70ca945c4e9b7cecfc26da9ceed9c5c2b7bda110
yossep237 <yossep237@gmail.com>
2021-12-09 12:12:03 +0000

    Application factory added

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 """ Application startup file """
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello World by Flask"
-
+# Local party import
+from Application import app
 
 if __name__ == "__main__":
     app.run()
