db16ccf06f565846c18ed6a89213068713b7d50e
yossep237 <yossep237@gmail.com>
2021-12-09 12:03:43 +0000

    My first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+""" Application startup file """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World by Flask"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
