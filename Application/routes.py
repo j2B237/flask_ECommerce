fe08636e4bb694f55985e4e5f378b0c847b68eee
yossep237 <yossep237@gmail.com>
2021-12-09 14:53:06 +0000

    add changes to base.html

--- Application/routes.py
+++ Application/routes.py
@@ -10,3 +10,28 @@ from Application import app
 @app.route("/")
 def index():
     return render_template("index.html", title="Sen Shop")
+
+
+@app.route("/account")
+def account():
+    return "Account view"
+
+
+@app.route("/wishlist")
+def wishlist():
+    return "wishlist view"
+
+
+@app.route("/cart")
+def cart():
+    return "cart view"
+
+
+@app.route("/checkout")
+def checkout():
+    return "checkout view"
+
+
+@app.route("/login")
+def login():
+    return "Login view"
