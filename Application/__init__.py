ca3472dc8d1ca9cc8c33dc7a658a10fad130bb8f
yossep237 <yossep237@gmail.com>
2021-12-09 12:21:47 +0000

    App parametize to development env

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,8 +1,13 @@
 """ Application factory """
 
+# Third party import
 from flask import Flask
 
+# Local party import
+from instance.config import config
+
 app = Flask(__name__)
+app.config.from_object(config["development"])
 
 
 @app.route("/")
